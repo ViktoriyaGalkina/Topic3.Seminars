31efcecf3a67fd84b9d4d22b60072cf66d112d06
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-03 21:30:21 +0300

    new

---
+++ Seminar_3.2/Task11.2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число 
+// и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+System.Console.WriteLine($"Случайное число: {number}");
+
+// System.Console.WriteLine($"Число без второй цифры : {number / 100}{number % 10}");
+
+number = number/100*10 + number%10;
+
+System.Console.WriteLine(number);
---
+++ Seminar_3.2/Task11/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+
+Random rand = new Random();
+int number = rand.Next(10, 100); // [10; 100)
+
+System.Console.WriteLine($"Случайное число: {number}");
+
+if (number / 10 > number % 10)
+{
+    System.Console.WriteLine($"Левое число больше и равно {number / 10}");
+}
+else if (number / 10 < number % 10)
+{
+    System.Console.WriteLine($"Правое число больше и равно {number % 10}");
+}
+else{
+    System.Console.WriteLine("Числа равны");
+}
+
+// /

2b2338a6f79439dafe9a87f5471d1cf353d6fffa
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-03 21:43:41 +0300

    new

---
+++ Seminar_3.3/Task17/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int InputNumber(string str)
+{
+    System.Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}                                            // string, bool, int[], double[], string[], void 
+
+void FindQuarter(int a, int b)
+{
+    if (a > 0 && b > 0)
+    {
+        System.Console.WriteLine("Номер четверти 1");
+    }
+    else if (a < 0 && b > 0)
+    {
+        System.Console.WriteLine("Номер четверти 2");
+    }
+    else if (a < 0 && b < 0)
+    {
+        System.Console.WriteLine("Номер четверти 3");
+    }
+    else if (a > 0 && b < 0)
+    {
+        System.Console.WriteLine("Номер четверти 4");
+    }
+    else
+    {
+        System.Console.WriteLine("Точка лежит в начале координат");
+    }
+}
+
+
+int x = InputNumber("Введите координату Х: ");
+int y = InputNumber("Введите координату Y: ");
+
+FindQuarter(x, y);
+FindQuarter(-7, 4);
---
+++ Seminar_3.3/Task20/Program.cs
@@ -0,0 +1,49 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+// Math.Pow(число,колтчество квадратов)
+// Math.Sqrt(квадратный корень из указанного числа)
+
+int InputNumber(string str) 
+{
+    System.Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int x1 = InputNumber("Введите координату Х1: ");
+int y1 = InputNumber("Введите координату Y1: ");
+
+int x2 = InputNumber("Введите координату Х2: ");
+int y2 = InputNumber("Введите координату Y2: ");
+
+// System.Console.WriteLine(Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2)));
+
+System.Console.WriteLine(Math.Round(Math.Pow(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2),0.5),3));
+
+
+
+// Второе решение:
+
+// Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+// int InputNumber(string str)
+// {
+//     System.Console.Write(str);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int x1 = InputNumber("Введите координату x первой точки: ");
+// int y1 = InputNumber("Введите координату y первой точки: ");
+// int x2 = InputNumber("Введите координату x второй точки: ");
+// int y2 = InputNumber("Введите координату y второй точки: ");
+
+// double result = Math.Round(Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2)), 3);
+
+// System.Console.WriteLine($"Расстояние = {result}");

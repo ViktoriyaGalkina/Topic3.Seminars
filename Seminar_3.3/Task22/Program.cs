3ce31ee432436f866f9208c8135a18696904f7ea
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-03 22:53:45 +0300

    Added task 22

--- Seminar_3.3/Task22/Program.cs
+++ Seminar_3.3/Task22/Program.cs
@@ -5,19 +5,26 @@
 // 5 -> 1, 4, 9, 16, 25.
 // 2 -> 1,4
 
-System.Console.Write("Введите число N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-
-for (int count = 1; count <= N; count++)
+void PrintSquares(int x)
 {
-    System.Console.Write(Math.Pow(count, 2) + ", ");
+for (int i = 1; i <= x; i++)
+{
+  System.Console.Write(Math.Pow(i, 2) + ", ");
+}
 }
 
+System.Console.WriteLine("Введите число N");
+int a = Convert.ToInt32(Console.ReadLine());
 
-// int count = 1;
+System.Console.WriteLine();
 
-// while (count <= N)
+PrintSquares(a);
+
+
+// System.Console.Write("Введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// for (int count = 1; count <= N; count++)
 // {
 //     System.Console.Write(Math.Pow(count, 2) + ", ");
-//     count++;
 // }

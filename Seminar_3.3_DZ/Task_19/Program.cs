31f950c8c2f01d137dafc3a27f37915e44435697
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-04 02:04:33 +0300

    Added task 21

--- Seminar_3.3_DZ/Task_19/Program.cs
+++ Seminar_3.3_DZ/Task_19/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задача 19: Напишите программу, которая принимает 
+// на вход пятизначное число и проверяет, является ли 
+// оно палиндромом.
+
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+// git remote set-url origin -- ссылку перезаписать на репозиторий
+
+
+// int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray(); 
+// Вывод массива одной строкой в массив целочисленных чисел.
+
+// или
+
+// System.Console.Write(string.Join("-", arr));
+
+
+System.Console.WriteLine("Введите пятизначное число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+System.Console.Write(string.Join(", ", array));
+// string[] array= number.Length;
+
+
+if (number > 9999 && number < 100000)
+{
+    if (array[1] == array[5] && array[2] == array[4])
+    {
+        System.Console.WriteLine("Число является палиндромом!");
+    }
+    else
+    {
+        System.Console.WriteLine("Вы ввели не пятизначное число(");
+    }
+}

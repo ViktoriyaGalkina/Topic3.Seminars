8e399d523fe7eef9aaed510013669baeacc17103
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-03 21:41:29 +0300

    new

---
+++ Seminar_3.2_DZ/Task18/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 18. Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+System.Console.Write("Введите номер четверти: ");
+int q = Convert.ToInt32(Console.ReadLine());
+
+string [] array= {"x > 0, y > 0","x < 0, y > 0","x < 0, y < 0","x > 0, y < 0","Такой четверти нет"};
+
+
+if (q == 1)
+{
+System.Console.WriteLine(array[0]);
+}
+else if (q == 2)
+{
+System.Console.WriteLine(array[1]);
+}
+else if (q == 3)
+{
+System.Console.WriteLine(array[2]);
+}
+else if (q == 4)
+{
+System.Console.WriteLine(array[3]);
+}
+else
+System.Console.WriteLine(array[4]);
+
+
+// System.Console.WriteLine("Введите четверть");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Diapazon(x);
+// void Diapazon(int a)
+// {
+
+//     if (a == 1)
+//     {
+//         System.Console.WriteLine("Диапазон x ∈ (0;+∞], y ∈ (0;+∞]");
+//     }
+//     else if (a == 2)
+//     {
+//         System.Console.WriteLine("Диапазон x ∈ (0;-∞], y ∈ (0;+∞]");
+//     }
+//     else if (a == 3)
+//     {
+//         System.Console.WriteLine("Диапазон x ∈ (0;-∞], y ∈ (0;-∞]");
+//     }
+//     else if (a == 4)
+//     {
+//         System.Console.WriteLine("Диапазон x ∈ (0;+∞], y ∈ (0;-∞]");
+//     }
+//     else
+//     {
+//         System.Console.WriteLine("Диапазон не существует");
+//     }
+
+// }
+
---
+++ Seminar_3.2_DZ/Task22/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 22. 
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+System.Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int count = 1; count <= N; count++)
+{
+    System.Console.Write(Math.Pow(count, 2) + ", ");
+}
+
+
+// int count = 1;
+
+// while (count <= N)
+// {
+//     System.Console.Write(Math.Pow(count, 2) + ", ");
+//     count++;
+// }

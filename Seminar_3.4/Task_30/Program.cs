68bf06682a0ca9347acf2ad648fa60b9649b511d
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-06 21:20:46 +0300

    task 30

--- Seminar_3.4/Task_30/Program.cs
+++ Seminar_3.4/Task_30/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+// // [1,0,1,1,0,1,0,0]
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+
+int[] FillArray(int size)
+{
+    int[] arrN = new int[size];
+    for (int i = 0; i < 8; i++)
+    {
+        arrN[i] = new Random().Next(0, 2);
+    }
+    return arrN;
+}
+
+int[] myArray = FillArray(8);
+PrintArray(myArray);

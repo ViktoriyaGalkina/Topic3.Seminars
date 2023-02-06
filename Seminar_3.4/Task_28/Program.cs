c13c17f1d85fe413be19cfb3416cc8797cfce44c
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-06 20:55:48 +0300

    task 26

---
+++ Seminar_3.4/Task_28/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N 
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int GetNumber(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int num = GetNumber("Введите число: ");
+int result = 1;
+for (int i =1; i<=num; i++)
+{
+result = result*i;
+}
+
+System.Console.WriteLine($"Произведение чисел от 1 до {num} = {result}");

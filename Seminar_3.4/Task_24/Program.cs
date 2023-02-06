07141a342a2b926e5e16a002d91477d4a5994871
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-06 20:39:11 +0300

    task 24

--- Seminar_3.4/Task_24/Program.cs
+++ Seminar_3.4/Task_24/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int GetNumber(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSumElements(int A)
+{
+    int result = 0;
+
+    for (int i = 1; i <= A; i++)
+    {
+        result += i; // result = result + i;
+    }
+    
+    return result;
+}
+
+int num = GetNumber("Введите число А: ");
+int res = GetSumElements(num);
+
+System.Console.WriteLine($"Сумма чисел от 1 до {num} = {res}");

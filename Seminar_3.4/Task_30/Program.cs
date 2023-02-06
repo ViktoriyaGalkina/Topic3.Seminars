b9f3d0bda734dc23ed03874aa5fa2fb7b5445179
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-06 21:46:51 +0300

    task 30

--- Seminar_3.4/Task_30/Program.cs
+++ Seminar_3.4/Task_30/Program.cs
@@ -7,24 +7,68 @@
 // заполненный нулями и единицами в случайном порядке.
 // // [1,0,1,1,0,1,0,0]
 
-void PrintArray(int[] array)
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         System.Console.Write($"{array[i]} ");
+//     }
+// }
+
+// int[] FillArray(int size)
+// {
+//     int[] arrN = new int[size];
+//     for (int i = 0; i < 8; i++)
+//     {
+//         arrN[i] = new Random().Next(0, 2);
+//     }
+//     return arrN;
+// }
+
+// int[] myArray = FillArray(8);
+// PrintArray(myArray);
+
+
+
+
+int[] GetArray()
 {
-    int count = array.Length;
-    for (int i = 0; i < count; i++)
-    {
-        System.Console.Write($"{array[i]} ");
-    }
+var arr = new int[8];
+var rand = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rand.Next(0, 2);
+}
+return arr;
 }
 
-int[] FillArray(int size)
+void PrintArray(int[] arr)
 {
-    int[] arrN = new int[size];
-    for (int i = 0; i < 8; i++)
-    {
-        arrN[i] = new Random().Next(0, 2);
-    }
-    return arrN;
+System.Console.WriteLine("["+string.Join(", ", arr)+"]");
 }
 
-int[] myArray = FillArray(8);
+var myArray = GetArray();
+PrintArray(myArray);
+
+
+
+
+// ---------- Версия 2
+void GetArray(int[] arr)
+{
+var rand = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rand.Next(0, 2);
+}
+}
+
+void PrintArray(int[] arr)
+{
+System.Console.WriteLine("["+string.Join(", ", arr)+"]");
+}
+
+int[] myArray = new int[8];
+GetArray(myArray);
 PrintArray(myArray);

abfc06ad8f8ce990901176cd8cb20c15685ab288
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-06 20:56:11 +0300

    task 26

---
+++ Seminar_3.4/Task_26/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт 
+// количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+string GetNumber(string text)
+{
+    System.Console.Write(text);
+    return Console.ReadLine();
+}
+
+string A = GetNumber("Введите число: ");
+int lenght = A.Length;
+System.Console.WriteLine($"Количество цифр в числе: " + lenght);

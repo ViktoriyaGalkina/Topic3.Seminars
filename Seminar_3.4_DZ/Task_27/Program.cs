aad428cd2df17378e318a8f5e54b55f64773f0f5
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-08 04:22:27 +0300

    task 27 3 versions%

--- Seminar_3.4_DZ/Task_27/Program.cs
+++ Seminar_3.4_DZ/Task_27/Program.cs
@@ -5,38 +5,70 @@
 // 9012 -> 12
 
 
-// 1 версия (математическая).
+// 1 версия (математическая).(не работает с числами,которые начинаются с 1)
+
+int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+int Sum(int number)
+{
+    int SumIndex = 0;
+    for (int i = 0; i < number; i++)
+    {
+        SumIndex = SumIndex + number % 10;
+        number = number / 10;
+    }
+    return SumIndex;
+}
+
+int N = GetNumber("Введите число: ");
+System.Console.WriteLine($"Сумма цифр в числе равна: {Sum(N)} ");
+
+
+
+// 2.1 версия(через массив)(1 вариант)  // Работает,но без второй функции.
+
+// string GetNumber(string text)
+// {
+//     System.Console.WriteLine(text);
+//     return Console.ReadLine();
+// }
+
+// string N = GetNumber("Введите число: ");
+
+// int res = 0;
+// for (int i = 0; i < N.Length; i++)
+// {
+//     int Converted = Convert.ToInt32(Convert.ToString(N[i]));
+//     res = res + Converted;
+// }
+// System.Console.WriteLine($"Сумма цифр в числе равна: " + res);
+
+
+
+// 2.2 версия(через массив)(2 вариант - если все преобразую в функции - выдает ошибку : 
+// (73,25): error CS1503: Аргумент 1: не удается преобразовать из "int" в "int[]".
+
 
 // int GetNumber(string text)
 // {
 //     System.Console.WriteLine(text);
-//     return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+//     return Convert.ToInt32(Console.ReadLine());
 // }
 
-// int Sum(int number)
+// int SumNumbers(int[] len)
 // {
-//     int SumIndex = 0;
-//     for (int i = 0; i <= number; i++)
+//     var res = 0;
+//     for (int i = 0; i < len.Length; i++)
 //     {
-//         SumIndex = SumIndex + number % 10;
-//         number = number / 10;
+//         res = res + len[i];
 //     }
-//     return SumIndex;
+//     return res;
 // }
 
 // int N = GetNumber("Введите число: ");
-// System.Console.WriteLine($"Сумма цифр в числе равна: {Sum(N)} ");
-
-
-
-// 2 версия(через массив).
-
-string GetNumber(string text)
-{
-    System.Console.WriteLine(text);
-    return Convert.ToInt32(Console.ReadLine());
-}
-
-string N = GetNumber("Введите число: ");
-int len = N.Length;
-System.Console.WriteLine($"Сумма цифр в числе равна: {len[0]+len[N]} ");
+// int result = SumNumbers(N);
+// System.Console.WriteLine($"Сумма цифр в числе равна: " + result);

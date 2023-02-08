31a15352a17811060835b9d726e81a8c4aa1c99f
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-08 18:34:27 +0300

    task 26

--- Seminar_3.4/Task_26/Program.cs
+++ Seminar_3.4/Task_26/Program.cs
@@ -4,12 +4,26 @@
 // 78 -> 2
 // 89126 -> 5
 
-string GetNumber(string text)
-{
-    System.Console.Write(text);
-    return Console.ReadLine();
-}
+// string GetNumber(string text)
+// {
+//     System.Console.Write(text);
+//     return Console.ReadLine();
+// }
 
-string A = GetNumber("Введите число: ");
-int lenght = A.Length;
-System.Console.WriteLine($"Количество цифр в числе: " + lenght);
+// string A = GetNumber("Введите число: ");
+// int lenght = A.Length;
+// System.Console.WriteLine($"Количество цифр в числе: " + lenght);
+
+
+
+// 2 вариант с TryParse.
+System.Console.Write("Введите число N: ");
+string? vvod = Console.ReadLine();
+if (int.TryParse(vvod, out int number))
+{
+System.Console.Write(Math.Pow(number, 3));
+}
+else
+{
+System.Console.WriteLine("Введено не число!");
+}

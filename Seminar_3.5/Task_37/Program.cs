7f64d9a29c761a1ceb266b721ed92b2f50ce91d8
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-09 21:55:55 +0300

    task 37

---
+++ Seminar_3.5/Task_37/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+// Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1); 
+    }
+    return array;
+}
+
+int[] GetResult(int[] array)
+{
+    int size;
+    if (array.Length % 2 == 0)
+        size = array.Length / 2;
+    else
+        size = array.Length / 2 + 1;
+
+    int[] arrResult = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arrResult[i] = array[i] * array[array.Length - 1 - i];
+    }
+    if (array.Length % 2 != 0)
+    {
+        arrResult[arrResult.Length - 1] = array[size-1];
+    }
+
+    return arrResult;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+
+int[] arr = GenerateArray(7, 1, 10);
+PrintArray(arr);
+int[] newArr = GetResult(arr);
+PrintArray(newArr);
+
+// System.Console.WriteLine(GetResult(arr));

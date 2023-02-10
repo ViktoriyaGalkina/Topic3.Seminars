fe9e8a259885b44b7068f4064a7fa86f3aaec0d8
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-10 04:13:06 +0300

    task 38

--- Seminar_3.5_DZ/Task_38_DZ/Program.cs
+++ Seminar_3.5_DZ/Task_38_DZ/Program.cs
@@ -4,22 +4,42 @@
 // [3 7 22 2 78] -> 76
 
 
-double[] GetArray(double size, double leftRange, double rightRange)
+// double[] GetArray(int size)
+// {
+//     double[] array = new double[size];
+//     var rand = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] =  Math.Round(rand.NextDouble()*100,2);
+//     }
+//     return array;
+// }
+
+// void PrintArray(double[] array)
+// {
+//     System.Console.WriteLine("[" + string.Join("; ", array) + "]");
+// }
+
+// double[] MyArray = GetArray(4);
+// PrintArray(MyArray);
+
+
+
+double[] GetArray(int size, int leftRange, int rightRange)
 {
     double[] array = new double[size];
     var rand = new Random();
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = rand.Next(leftRange, rightRange + 1);
+        array[i] =  Math.Round((rand.Next(leftRange, rightRange)+rand.NextDouble()),2);
     }
     return array;
 }
 
 void PrintArray(double[] array)
 {
-    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+    System.Console.WriteLine("[" + string.Join("; ", array) + "]");
 }
 
-
-double[] MyArray = GetArray(4, -100, 100);
+double[] MyArray = GetArray(4,-10,10);
 PrintArray(MyArray);

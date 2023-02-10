03aa5ff84bedaf5085dcd92b820a11a6c4795780
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-10 18:44:12 +0300

    task 27

--- Seminar_3.5_DZ/Task_38_DZ/Program.cs
+++ Seminar_3.5_DZ/Task_38_DZ/Program.cs
@@ -20,8 +20,24 @@ void PrintArray(double[] array)
     System.Console.WriteLine("[" + string.Join("; ", array) + "]");
 }
 
+
 double[] MyArray = GetArray(4, 0, 20);
 PrintArray(MyArray);
+
+
+int Max = 0;
+int[] arr = new int[MyArray];
+for(int i = 0; i < arr.Length-1; i++) //-1
+{
+    int minPosition = j;
+    for(int j = i+1;j< arr.Length; j++)
+    {
+         if (arr[j] < arr[minPosition])
+            {
+                minPosition = j;
+            }
+    }
+}
 
 
 
@@ -32,7 +48,7 @@ PrintArray(MyArray);
 //         int minPosition = i;
 //         for (int j = i + 1; j < array.Length; j++)
 //         {
-//             if (array[j] < array[minPosition]) minPosition = j;
+//             if (array[j] < array[minPosition])
 //             {
 //                 minPosition = j;
 //             }

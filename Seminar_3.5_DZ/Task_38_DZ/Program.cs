af72b94edadc7d09c7edf3bc1f9d879b345b8492
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-10 04:32:48 +0300

    task 38

--- Seminar_3.5_DZ/Task_38_DZ/Program.cs
+++ Seminar_3.5_DZ/Task_38_DZ/Program.cs
@@ -1,7 +1,57 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. 
-// Найдите разницу между максимальным и минимальным элементов массива.
+// Найдите разницу между максимальным и минимальным элементом массива.
 
 // [3 7 22 2 78] -> 76
+
+
+double[] GetArray(int size, int leftRange, int rightRange)
+{
+    double[] array = new double[size];
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round((rand.Next(leftRange, rightRange) + rand.NextDouble()), 2);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    System.Console.WriteLine("[" + string.Join("; ", array) + "]");
+}
+
+double[] MyArray = GetArray(4, 0, 20);
+PrintArray(MyArray);
+
+
+
+// void selectionSort(double[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//             {
+//                 minPosition = j;
+//             }
+//         }
+//         double temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
+
+
+
+
+
+// selectionSort(MyArray);
+
+
+
+
 
 
 // double[] GetArray(int size)
@@ -22,24 +72,3 @@
 
 // double[] MyArray = GetArray(4);
 // PrintArray(MyArray);
-
-
-
-double[] GetArray(int size, int leftRange, int rightRange)
-{
-    double[] array = new double[size];
-    var rand = new Random();
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] =  Math.Round((rand.Next(leftRange, rightRange)+rand.NextDouble()),2);
-    }
-    return array;
-}
-
-void PrintArray(double[] array)
-{
-    System.Console.WriteLine("[" + string.Join("; ", array) + "]");
-}
-
-double[] MyArray = GetArray(4,-10,10);
-PrintArray(MyArray);

fe16aee6ed511353ea73e4445a4fa63765623aea
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-16 19:58:36 +0300

    task 45

---
+++ Seminar_3.6/Task_45/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного копирования.
+
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+int[] CopyPaste(int[] array)
+{
+    int[] array2 = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array2[i] = array[i];
+    }
+    return array2;
+
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+int[] arr = GenerateArray(5, 0, 100);
+PrintArray(arr);
+int[] arrCopy = CopyPaste(arr);
+PrintArray(arrCopy);

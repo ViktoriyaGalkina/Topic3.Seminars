86a995389d6062cd32ccde9fef1597daec14a5cf
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-18 14:22:15 +0300

    task 44

--- Seminar_3.6/Task_44/Program.cs
+++ Seminar_3.6/Task_44/Program.cs
@@ -8,15 +8,14 @@
 System.Console.WriteLine("Введите число n: ");
 int Fibonachi1 = 0;
 int Fibonachi2 = 1;
-int FibonachiN;
 
 
 int n = Convert.ToInt32(System.Console.ReadLine());
 System.Console.Write("0 1 ");
 for (int i = 0; i < n - 2; i++)
 {
-int sum = Fibonachi1 + Fibonachi2;
-System.Console.Write(sum + " ");
-Fibonachi1 = Fibonachi2;
-Fibonachi2 = sum;
+    int sum = Fibonachi1 + Fibonachi2;
+    System.Console.Write(sum + " ");
+    Fibonachi1 = Fibonachi2;
+    Fibonachi2 = sum;
 }

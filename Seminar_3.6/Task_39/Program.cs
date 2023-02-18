1f0c74b064517c156cb9996200a314cecb5bdd3a
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-18 22:33:31 +0300

    task 52

--- Seminar_3.6/Task_39/Program.cs
+++ Seminar_3.6/Task_39/Program.cs
@@ -6,30 +6,30 @@
 
 int[] GenerateArray(int size, int minValue, int maxValue)
 {
-int[] arr = new int[size];
-Random rand = new Random();
+    int[] arr = new int[size];
+    Random rand = new Random();
 
-for (int i = 0; i < arr.Length; i++)
-{
-arr[i] = rand.Next(minValue, maxValue + 1);
-}
-return arr;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(minValue, maxValue + 1);
+    }
+    return arr;
 }
 
 void PrintArray(int[] arr)
 {
-System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
 }
 
 void ReverseArray(int[] arr)
 {
-int temp;
-for(int i = 0; i < arr.Length/2; i++)
-{
-temp = arr[i];
-arr[i] = arr[arr.Length - 1 - i];
-arr[arr.Length - 1 - i] = temp;
-}
+    int temp;
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    }
 }
 
 var array = GenerateArray(5, 0, 10);

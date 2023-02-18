8867965618346c60c4a8b364b168609f0fe82eaf
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-18 14:19:06 +0300

    task 51

--- Seminar_3.7/Task_51/Program.cs
+++ Seminar_3.7/Task_51/Program.cs
@@ -10,60 +10,60 @@
 
 
 
-int ReadInt(string text)
-{
-    System.Console.Write(text);
-    return Convert.ToInt32(Console.ReadLine());
-}
+// int ReadInt(string text)
+// {
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
 
-int[,] GenerateMatrix(int rows, int cols)
-{
-    Random rand = new Random();
-    int[,] matrix = new int[rows, cols];
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = rand.Next(0, 10);
-        }
-    }
-    return matrix;
-}
+// int[,] GenerateMatrix(int rows, int cols)
+// {
+//     Random rand = new Random();
+//     int[,] matrix = new int[rows, cols];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rand.Next(0, 10);
+//         }
+//     }
+//     return matrix;
+// }
 
-void PrintMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            System.Console.Write(matrix[i, j] + "\t");
-        }
-        System.Console.WriteLine();
-    }
-}
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             System.Console.Write(matrix[i, j] + "\t");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
 
-int SumElements(int[,] matrix)
-{
-    int SumElements = 0;
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            if (i == j)
-            {
-                SumElements += matrix[i, j];
-            }
-        }
-    }
-    return SumElements;
-}
+// int SumElements(int[,] matrix)
+// {
+//     int SumElements = 0;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (i == j)
+//             {
+//                 SumElements += matrix[i, j];
+//             }
+//         }
+//     }
+//     return SumElements;
+// }
 
-int rows = ReadInt("Введите количество строк матрицы: ");
-int cols = ReadInt("Введите количество столбцов матрицы: ");
+// int rows = ReadInt("Введите количество строк матрицы: ");
+// int cols = ReadInt("Введите количество столбцов матрицы: ");
 
-var myMatrix = GenerateMatrix(rows, cols);
-PrintMatrix(myMatrix);
-System.Console.WriteLine(SumElements(myMatrix));
+// var myMatrix = GenerateMatrix(rows, cols);
+// PrintMatrix(myMatrix);
+// System.Console.WriteLine(SumElements(myMatrix));
 
 
 

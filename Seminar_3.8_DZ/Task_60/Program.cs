7130a536d85c62f34550938ef2d80845ba2daa8f
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-21 23:53:09 +0300

    task 60

---
+++ Seminar_3.8_DZ/Task_60/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,,] GenerateMatrix(int rows, int cols, int heights)
+{
+    Random rand = new Random();
+    int[,,] matrix = new int[rows, cols, heights];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = rand.Next(10,100);
+            }
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                System.Console.Write($"{matrix[i, j, k]}({i},{j},{k}) " + "\t");
+            }
+        }
+    }
+}
+
+int rows = ReadInt("Введите количество строк матрицы: ");
+int cols = ReadInt("Введите количество столбцов матрицы: ");
+int heights = ReadInt("Введите высоту матрицы: ");
+var myMatrix = GenerateMatrix(rows, cols, heights);
+PrintMatrix(myMatrix);

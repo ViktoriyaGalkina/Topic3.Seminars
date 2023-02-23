051bf02fe7ad20154673630b43b645c80e42f9eb
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-23 21:04:39 +0300

    task 69

---
+++ Seminar_3.9/Task_69/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+System.Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(GetResult(a, b, 1));
+
+int GetResult (int numA, int numB, int res)
+{
+    numB--;
+    if (numB >= 0)
+    {
+        res = res * numA;
+        return GetResult (numA, numB, res);
+    }
+    else
+    {
+        return res;
+    }
+}

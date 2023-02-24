8c58dff94dde376807e9bf8c0600891aacf3d313
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-24 20:39:20 +0300

    task 68

--- Seminar_3.9/Task_69/Program.cs
+++ Seminar_3.9/Task_69/Program.cs
@@ -2,45 +2,42 @@
 // A = 3; B = 5 -> 243 (3⁵)
 // A = 2; B = 3 -> 8
 
-// System.Console.Write("Введите число A: ");
-// int a = Convert.ToInt32(Console.ReadLine());
-// System.Console.Write("Введите число B: ");
-// int b = Convert.ToInt32(Console.ReadLine());
-// System.Console.WriteLine(GetResult(a, b, 1));
+System.Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(GetResult(a, b, 1));
 
-// int GetResult (int numA, int numB, int res)
+int GetResult (int numA, int numB, int res)
+{
+    numB--;
+    if (numB >= 0)
+    {
+        res = res * numA;
+        return GetResult (numA, numB, res);
+    }
+    else
+    {
+        return res;
+    }
+}
+
+
+// int ReadInt(string text)
 // {
-//     numB--;
-//     if (numB >= 0)
-//     {
-//         res = res * numA;
-//         return GetResult (numA, numB, res);
-//     }
-//     else
-//     {
-//         return res;
-//     }
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
 // }
 
+// int Power(int n, int m)
+// {
+//     if (m == 1)
+//     {
+//         return n;
+//     }
+//     return n * Power(n, m--);
+// }
 
-int ReadInt(string text)
-{
-    System.Console.Write(text);
-    return Convert.ToInt32(Console.ReadLine());
-}
-
-int Power(int n, int m)
-{
-
-    if (m == 1)
-        return n;
-
-    return n * Power(n, m--);
-
-}
-
-int n = ReadInt("Введите N: ");
-int m = ReadInt("Введите M: ");
-
-
-System.Console.WriteLine(Power(n, m));
+// int n = ReadInt("Введите N: ");
+// int m = ReadInt("Введите M: ");
+// System.Console.WriteLine(Power(n, m));
---
+++ Seminar_3.9_DZ/Task_68/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Akkerman(int M, int N)
+{
+    if (M == 0)
+    {
+        return N + 1;
+    }
+    else if (M > 0 && N == 0)
+    {
+        return Akkerman(M - 1, 1);
+    }
+    else if (M > 0 && N > 0)
+    {
+        return Akkerman(M-1,Akkerman(M,N-1));
+    }
+    return Akkerman(M,N);
+}
+
+
+int m = ReadInt("Введите неотрицательное число M: ");
+int n = ReadInt("Введите неотрицательное число N: ");
+System.Console.WriteLine($"A({m},{n}) = {Akkerman(m,n)}");
+
+
+// Console.WriteLine("Введите начальное число M:");
+// int numberM = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите начальное число N:");
+// int numberN = int.Parse(Console.ReadLine());
+
+// ///Метод вычисления функции Аккермана:
+// int AckermannFunction (int numberM, int numberN)
+// {
+//     if (numberM == 0) return numberN + 1;
+//     if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
+//     if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+// return AckermannFunction(numberM, numberN);
+// }
+
+// Console.WriteLine($"Функция Аккермана для чисел A({numberM},{numberN}) = {AckermannFunction(numberM, numberN)}");

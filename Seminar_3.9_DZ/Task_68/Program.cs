f60f51aefa9739623ba4c4dcf4191e303bf2267f
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-02-24 20:39:44 +0300

    task 68

--- Seminar_3.9_DZ/Task_68/Program.cs
+++ Seminar_3.9_DZ/Task_68/Program.cs
@@ -30,21 +30,3 @@ int Akkerman(int M, int N)
 int m = ReadInt("Введите неотрицательное число M: ");
 int n = ReadInt("Введите неотрицательное число N: ");
 System.Console.WriteLine($"A({m},{n}) = {Akkerman(m,n)}");
-
-
-// Console.WriteLine("Введите начальное число M:");
-// int numberM = int.Parse(Console.ReadLine());
-
-// Console.WriteLine("Введите начальное число N:");
-// int numberN = int.Parse(Console.ReadLine());
-
-// ///Метод вычисления функции Аккермана:
-// int AckermannFunction (int numberM, int numberN)
-// {
-//     if (numberM == 0) return numberN + 1;
-//     if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
-//     if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
-// return AckermannFunction(numberM, numberN);
-// }
-
-// Console.WriteLine($"Функция Аккермана для чисел A({numberM},{numberN}) = {AckermannFunction(numberM, numberN)}");
